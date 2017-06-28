aed56beded0b4f72bc92bdc15dd61c8adab6ad4e
rsoni123 <Amaya123>
2017-06-28 19:31:16 -0400

    Code added

---
+++ app/src/main/java/com/example/pankaj/flickrbrowser/GetRawData.java
@@ -0,0 +1,9 @@
+package com.example.pankaj.flickrbrowser;
+
+/**
+ * Created by pankaj on 6/28/2017.
+ */
+public class GetRawData {
+    String URL = "https://api.flickr.com/services/" +
+            "feeds/photos_public.gne?format=json&nojsoncallback=1";
+}
