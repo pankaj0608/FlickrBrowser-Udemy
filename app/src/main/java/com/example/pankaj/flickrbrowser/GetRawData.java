7265143a104febda58b444852f983f971d4b79c3
rsoni123 <Amaya123>
2017-06-28 20:05:31 -0400

    Code added

--- app/src/main/java/com/example/pankaj/flickrbrowser/GetRawData.java
+++ app/src/main/java/com/example/pankaj/flickrbrowser/GetRawData.java
@@ -1,9 +1,20 @@
 package com.example.pankaj.flickrbrowser;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by pankaj on 6/28/2017.
  */
-enum DownloadStatus {IDLE, PROCESSING, NOT_INITIALSED, FAILED_OR_EMPTY, OK}
+enum DownloadStatus {
+    IDLE, PROCESSING, NOT_INITIALSED, FAILED_OR_EMPTY, OK
+}
 
 public class GetRawData {
     String URL = "https://api.flickr.com/services/" +
@@ -33,4 +44,59 @@ public class GetRawData {
         return mDownloadStatus;
     }
 
+    public class DownloadRawData extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            if (params == null) {
+                return null;
+            }
+
+            try {
+
+                URL url = new URL(params[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                return buffer.toString();
+
+            } catch (Exception e) {
+                Log.e(LOG_TAG, e.getMessage());
+                return null;
+                
+            } finally {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+
+                if (reader != null)
+                    try {
+                        reader.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+            }
+
+
+        }
+    }
 }
